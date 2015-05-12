8f3391abda8ca6dbd13228314620e8a56a289e01
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2015-05-11 20:27:50 -0400

    huge compactification

--- lisp-format.lisp
+++ lisp-format.lisp
@@ -2,51 +2,11 @@
 
 ;;; Lines ;;;
 
-#| line:
-(prn (proc (lin "" "test" "" "" "test"))) ->
-testtest
-|#
+(defbuil lin lns flns lvl)
+(def ind (n . a) (mkdat 'ind a {n n}))
+(def wind (n . a) (mkdat 'wind a {n n}))
 
-(def lin a
-  (mkdat 'lin a))
-
-#| lines:
-(prn (proc (lns "" "test" "" "" "test"))) ->
-
-test
-
-
-test
-|#
-
-(def lns a
-  (mkdat 'lns a))
-
-#| fresh lines:
-(prn (proc (flns "" "test" "" "" "test"))) ->
-test
-test
-|#
-
-(def flns a
-  (mkdat 'flns a))
-
-#| level:
-(prn (proc (lin "test" (lvl "test" "" "abc")))) ->
-testtest
-    
-    abc
-|#
-
-(def lvl a
-  (mkdat 'lvl a))
-
-#| level with indent on next lines:
-(prn (proc (lin "test" (lvlind 3 "testing" "abc" "def")))) ->
-testtesting
-       abc
-       def
-
+#|
 (def lvlind (n . a)
   (mkdat 'lvlind a {n n}))
 |#
@@ -54,19 +14,49 @@ testtesting
 (mac lvlind (n fst . rst)
   `(lvl ,fst (ind ,n ,@rst)))
 
-#| indent:
+; proc only uses dat property
+(def note (a opt)
+  (mkdat 'note a opt))
+
+#|
+line:
+(prn (proc (lin "" "test" "" "" "test"))) ->
+testtest
+----------------------
+lines:
+(prn (proc (lns "" "test" "" "" "test"))) ->
+
+test
+
+
+test
+--------------------
+fresh lines:
+(prn (proc (flns "" "test" "" "" "test"))) ->
+test
+test
+-------------------
+level:
+(prn (proc (lin "test" (lvl "test" "" "abc")))) ->
+testtest
+    
+    abc
+----------------------
+level with indent on next lines:
+(prn (proc (lin "test" (lvlind 3 "testing" "abc" "def")))) ->
+testtesting
+       abc
+       def
+-----------------
+indent:
 (prn (proc (lns "test" (ind 3 "testing" (ind 2 "abc" "def")) "hey"))) ->
 test
    testing
      abc
      def
 hey
-|#
-
-(def ind (n . a)
-  (mkdat 'ind a {n n}))
-
-#| with indent:
+-------------------
+with indent:
 (prn (proc (lns "test" (ind 3 "testing" (wind 2 "abc" "def") "what") "hey"))) ->
 test
    testing
@@ -74,38 +64,16 @@ test
   def
    what
 hey
+---------------
 |#
 
-(def wind (n . a)
-  (mkdat 'wind a {n n}))
-
-; return object: proc only uses dat property
-(def note (a opt)
-  (mkdat 'note a {opt opt}))
-
-; applies f to dat property of a
-(def calldat (f a)
-  (mkdat (typ a) (f (dat a)) a))
-
-#|(over dsp (a)
-  (sup (trans a)))
-
-(def trans (a)
-  (case a
-    obj? (case (typ a)
-           'lin `(lin ,@(trans (dat a)))
-           'lns `(lns ,@(trans (dat a)))
-           'ind `(ind ,(. a n) ,@(trans (dat a)))
-           'rt  `(rt ,(. a tp) ,(trans (dat a))))
-    lis? (map trans a)
-    a))|#
 
 ;;; Output lines ;;;
 
-(var *indlvl* 0)
-(var *begline* t)
-(var *linepos* 0)
-(var *indented* nil)
+(defvar *indlvl* 0
+        *begline* t
+        *linepos* 0
+        *indented* nil)
 
 ; don't send "\n" to emit
 ; *begline* is t after \n is printed til the first text is emitted
@@ -126,25 +94,19 @@ hey
 
 (def newln ()
   (pr "\n")
-  (= *linepos* 0)
-  (= *begline* t)
-  (= *indented* nil))
+  (reset *linepos* *begline* *indented*))
 
 (def freshln ()
   (unless *begline* (newln)))
 
 (def resetln ()
-  (= *indlvl* 0)
-  (= *linepos* 0)
-  (= *begline* t)
-  (= *indented* nil)
-  nil)
+  (reset *indlvl* *linepos* *begline* *indented*))
 
 ;;; Process lines ;;;
 
 (def proc (a)
   (resetln)
-  (tostr (proclin (lin a))))
+  (tostr (proc1 a)))
 
 ; process any type
 (def proc1 (a)
@@ -161,47 +123,26 @@ hey
 
 ; process lin objects
 (def proclin (a)
-  (each x (rflat (dat a))
-    (if x (proc1 x))))
+  (runexist proc1 (dat a)))
 
 ; process lns objects
 (def proclns (a)
-  (var fst t)
-  (each x (rflat (dat a))
-    (if (no x) (cont))
-    (if fst
-        (= fst nil)
-        (newln))
-    (proc1 x)))
+  (runexist proc1 (dat a)
+    btw (newln)))
 
 ; process flns objects
 (def procflns (a)
-  (var fst t)
-  (each x (rflat (dat a))
-    (if (no x) (cont))
-    (if fst (= fst nil)
-        (freshln))
-    (proc1 x)))
+  (runexist proc1 (dat a)
+    btw (freshln)))
 
 (def proclvl (a)
   (dyn *indlvl* (if *indented* *linepos* *indlvl*)
     (proclns a)))
 
-(mac dyn+= (a x . bd)
-  `(dyn ,a (+ ,a ,x) ,@bd))
-
-(mac dynzap (f a . bd)
-  `(dyn ,a (,f ,a) ,@bd))
-
 (def procind (a)
   (dyn+= *indlvl* (. a n)
-    (proclns (lns (dat a)))))
+    (proclns (lns @(dat a)))))
 
 (def procwind (a)
   (dyn *indlvl* (. a n)
-    (proclns (lns (dat a)))))
-
-(def rflat (a)
-  (if (no a) nil
-      (atm? (car a)) (cons (car a) (rflat (cdr a)))
-      (app (rflat (car a)) (rflat (cdr a)))))
+    (proclns (lns @(dat a)))))
